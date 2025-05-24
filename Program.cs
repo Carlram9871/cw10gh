9d8e0619ac7ea264976cc99709133262760ec8ee
Carlram9871 <carlram9871@gmail.com>
2025-05-24 16:46:24 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,10 +5,13 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-// Usa navegación con hash
+// Usamos navegación con hash (#) para compatibilidad con GitHub Pages
 builder.RootComponents.Add<App>("#app");
+
+// Inyectamos nuestra clase personalizada de navegación
 builder.Services.AddScoped<NavigationManager, CustomHashNavigationManager>();
 
+// Servicios
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<PreguntaService>();
 
