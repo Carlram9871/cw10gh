a91e1972a653d96f87e96335d2d95f6f16afcc49
Carlram9871 <carlram9871@gmail.com>
2025-05-24 18:09:30 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,11 +4,9 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Servicios
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<PreguntaService>();
 
