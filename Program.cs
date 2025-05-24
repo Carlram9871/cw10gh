c73d921e2ebc7777a36a3ec224c0d1f30fa19fe9
Carlram9871 <carlram9871@gmail.com>
2025-05-24 18:17:40 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,12 +2,12 @@ using CuestionarioWeb10;
 using CuestionarioWeb10.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+// Usamos navegación con hash (#) para compatibilidad con GitHub Pages
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
+// Inyectamos nuestra clase personalizada de navegación
+builder.Services.AddScoped<NavigationManager, CustomHashNavigationManager>();
+// Servicios
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<PreguntaService>();
-
 await builder.Build().RunAsync();
