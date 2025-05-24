6aa761cb53ce2463417c440c37bb277346fb0da7
Carlram9871 <carlram9871@gmail.com>
2025-05-24 16:39:39 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// Usa navegación con hash
 builder.RootComponents.Add<App>("#app");
+builder.Services.AddScoped<NavigationManager, CustomHashNavigationManager>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<PreguntaService>();
